7692f96ff0096ddc5b20cf55b9400be46572c348
emileyray <emileyray23@gmail.com>
2022-11-10 15:30:08 +0300

    add test project to test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 
 
 def main():
+    # lcov_path = 'test_project/coverage/lcov.info'
     lcov_path = 'coverage/lcov.info'
     total_lines = 0
     covered_lines = 0
@@ -10,7 +11,7 @@ def main():
         for line in f:
             if (line[0:2] == 'LF'):
                 total_lines += int(line[3:])
-            if (line[0:2] == 'HF'):
+            if (line[0:2] == 'LH'):
                 covered_lines += int(line[3:])
 
     percentage = round((covered_lines / total_lines), 2)
